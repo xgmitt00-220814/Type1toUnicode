28152112d36e9a5a21eb6a015aaa4e90e1571763
_xgmitt00_ <xgmitt00@gmail.com>
2024-05-12 21:22:53 +0200

    Add files via upload

--- hasher.py
+++ hasher.py
@@ -1,55 +1,79 @@
-import hashlib
-import os
-import json
-import subprocess
+from os import listdir, path
+from subprocess import run, CalledProcessError
+from json import load, JSONDecodeError
+from hashlib import sha256
 from argparse import ArgumentParser
 
 class Hash:
     @classmethod
-    def find_hash_in_json(cls, json_file_path, dir_path):
+    def find_hash_in_json(cls, json_file_path, dir_path, verbose):
         try:
-            # Načítanie hashov z JSON súboru do slovníka
+            # Load hashes from the JSON file into a dictionary
             with open(json_file_path, 'r') as file:
-                data = json.load(file)
-            # Získanie všetkých hashov z JSON súboru
+                data = load(file)
+
+            # Initialize a set to store hashes from the JSON file
             json_hashes = set()
+
+            # Define a recursive function to collect hashes from nested dictionaries
             def collect_hashes(year_dict):
                 for month, value in year_dict.items():
                     if isinstance(value, dict):
-                        collect_hashes(value)  # Rekurzívne prehľadávanie pre vnorené slovníky
+                        collect_hashes(value)  # Recursive search for nested dictionaries
                     else:
                         json_hashes.add(value)
 
+            # Iterate through each 'magazine' in the data to collect hashes
             for magazine in data["magazines"].values():
                 collect_hashes(magazine)
 
-            # Prechádzanie všetkých .pdf súborov v zadanom adresári a výpočet ich hashov
-            for filename in os.listdir(dir_path):
-                if filename.endswith(".pdf"):
-                    file_path = os.path.join(dir_path, filename)
-                    with open(file_path, 'rb') as file:
-                        file_content = file.read()
-                        file_hash = hashlib.sha256(file_content).hexdigest()
-                        # Porovnanie hashu súboru s hashmi z JSON súboru
-                        if file_hash in json_hashes:
-                            subprocess.run(['insert_table.exe', '-p', file_path, '-f', 'to_unicode.json'], check=True)
+            # Iterate over all .pdf files in the specified directory and calculate their hashes
+            for filename in listdir(dir_path):
+                if filename.endswith(".pdf"): # Check if the file extension is .pdf
+                    file_path = path.join(dir_path, filename) # Create the full path to the file
+                    with open(file_path, 'rb') as file: # Open the file in binary mode for hashing
+                        file_content = file.read() # Read the entire file content into memory
+                        file_hash = sha256(file_content).hexdigest() # Compute SHA-256 hash of the file content
+
+                        # Compare the file hash with the hashes from the JSON file
+                        if file_hash in json_hashes and verbose:
+                            run(['Type1toUnicode.exe', '-p', file_path, '-f', 'to_unicode.json', '-v'], check=True)
+                        elif file_hash in json_hashes:
+                            run(['Type1toUnicode.exe', '-p', file_path, '-f', 'to_unicode.json'], check=True)
                         else:
-                            print(f"Hash nenájdený")
+                            print(f"\033[33mHash not found for file: {filename}\033[0m")
 
         except FileNotFoundError:
-            print("Súbor nebol nájdený.")
-        except json.JSONDecodeError:
-            print("Súbor nie je platný JSON.")
-        except subprocess.CalledProcessError:
-            print("Chyba pri spúšťaní insert_table.exe.")
+            print("\033[31mFile does not exist.\033[0m")
+        except JSONDecodeError as e:
+            print("\033[31mError in parsing JSON file: {json_file_path}\n{e}\033[0m")
+        except CalledProcessError:
+            print("\033[31mError when running Type1toUnicode.exe\033[0m")
         except Exception as e:
-            print(f"Vyskytla sa chyba: {e}")
+            print(f"\033[31mAn exception occurred: {e}\033[0m")
 
 def main():
+    # Create an ArgumentParser object for handling command line arguments
     argparser = ArgumentParser()
-    argparser.add_argument('-d', '--pdf_dir', type=str, required=True)
+
+    # Add an argument for specifying the directory containing PDF files
+    # The argument '-d' is a shorthand, '--pdf_dir' is the full name, and it is required
+    argparser.add_argument('-d', '--pdf_dir', type=str, required=True, help='Defines the path to directory with .pdf files')
+
+    # Add an argument for specifying the JSON file containing hashes
+    # The argument '-f' is a shorthand, '--json_file' is the full name, and it is required
+    argparser.add_argument('-j', '--json_file', type=str, required=True, help='Defines the path to the .json file')
+
+    # Add a boolean argument for enabling verbose output
+    # This will not require a value; presence in the command line will set it to True
+    argparser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output (prints more information)')
     args = argparser.parse_args()
-    Hash.find_hash_in_json('magazine_hash.json', args.pdf_dir)
+
+    # Call the 'find_hash_in_json' method of the 'Hash' class
+    # 'magazine_hash.json' specifies the JSON file containing hashes,
+    # 'args.pdf_dir' is the directory to search for PDF files,
+    # 'args.verbose' controls the verbosity of the output
+    Hash.find_hash_in_json(args.json_file, args.pdf_dir, args.verbose)
 
 if __name__ == '__main__':
     main()
